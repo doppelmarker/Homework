bf8eb5429c41d74ce618caf791495fc3181705c3
doppelmarker <doppelmarker@gmail.com>
2021-09-16 12:33:43 +0300

    add session2 homework

---
+++ MarkKanaplianik/task1_7/convert_tuple/convert_tuple.py
@@ -0,0 +1,16 @@
+"""
+### Task 1.7
+Write a Python program to convert a given tuple of positive integers into an integer.
+Examples:
+```
+Input: (1, 2, 3, 4)
+Output: 1234
+```
+"""
+
+
+from functools import reduce
+
+
+def convert_tuple_to_integer(t):
+    return int(reduce(lambda n1, n2: f"{n1}{n2}", t))
