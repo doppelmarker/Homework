ea75b9d88bc1f071978a40cb1ef58efad05f104f
doppelmarker <doppelmarker@gmail.com>
2021-09-18 20:12:05 +0300

    add description to task4_2

--- MarkKanaplianik/session4/task4_2/check_palindrome/check_palindrome.py
+++ MarkKanaplianik/session4/task4_2/check_palindrome/check_palindrome.py
@@ -1,3 +1,11 @@
+"""
+### Task 4.2
+Write a function that check whether a string is a palindrome or not. Usage of
+any reversing functions is prohibited. To check your implementation you can use
+strings from [here](https://en.wikipedia.org/wiki/Palindrome#Famous_palindromes).
+"""
+
+
 def is_palindrome(s: str):
     for idx in range(0, len(s) // 2):
         if s[idx] != s[-idx - 1]:
