aa0f0418232681b4f4fc6cc89fd65c7fba474fca
doppelmarker <doppelmarker@gmail.com>
2021-09-19 17:53:20 +0300

    start task4_6

---
+++ MarkKanaplianik/task4_6/get_longest_word/get_longest_word.py
@@ -0,0 +1,16 @@
+"""
+### Task 4.6
+Implement a function `get_shortest_word(s: str) -> str` which returns the
+longest word in the given string. The word can contain any symbols except
+whitespaces (` `, `\n`, `\t` and so on). If there are multiple longest words in
+the string with a same length return the word that occures first.
+Example:
+```python
+
+>>> get_longest_word('Python is simple and effective!')
+'effective!'
+
+>>> get_longest_word('Any pythonista like namespaces a lot.')
+'pythonista'
+```
+"""
