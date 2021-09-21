a70046c58134753703dfc082cb495482d99d6a76
doppelmarker <doppelmarker@gmail.com>
2021-09-21 16:38:13 +0300

    simplify code

--- MarkKanaplianik/task5_1/sort_names_file/sort_names_file.py
+++ MarkKanaplianik/task5_1/sort_names_file/sort_names_file.py
@@ -11,23 +11,14 @@ Xavier
 """
 
 from pathlib import Path
-from typing import List, TextIO
+from typing import TextIO
 
 
 def read_file(reader: TextIO):
     return (line for line in reader)
 
 
-def write_file(writer: TextIO, lines: List[str]):
-    for line in lines:
-        writer.write(line)
-
-
 def work(path_file_read: Path, path_file_write: Path):
     with open(path_file_read) as reader:
-        lines = list(read_file(reader))
-
-    lines.sort()
-
-    with open(path_file_write, "w") as writer:
-        write_file(writer, lines)
+        with open(path_file_write, "w") as writer:
+            writer.writelines(sorted(reader))
