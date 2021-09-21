ac225637d9ee6d97bb7bcbf22486fb20ca803ba2
doppelmarker <doppelmarker@gmail.com>
2021-09-21 17:43:36 +0300

    add doctests for task5_2

--- MarkKanaplianik/task5_2/most_common_words/most_common_words.py
+++ MarkKanaplianik/task5_2/most_common_words/most_common_words.py
@@ -5,8 +5,13 @@ Use `data/lorem_ipsum.txt` file as a example.
 
 > NOTE: Remember about dots, commas, capital letters etc.
 
-# >>> most_common_words('lorem_ipsum.txt')
+>>> data_dir_path = Path(Path.cwd(), "../../..", "data")
+>>> filename = "lorem_ipsum.txt"
+>>> filepath = Path(data_dir_path, filename)
+>>> most_common_words(filepath)
 ['donec', 'etiam', 'aliquam']
+>>> most_common_words(filepath, 5)
+['donec', 'etiam', 'aliquam', 'aenean', 'maecenas']
 """
 
 from collections import Counter
@@ -19,9 +24,3 @@ def most_common_words(filepath, number_of_words=3):
         pattern = r"\w+"
         words_collection = Counter(findall(pattern, reader.read()))
         return [word[0] for word in words_collection.most_common(number_of_words)]
-
-
-if __name__ == "__main__":
-    data_dir_path = Path(Path.cwd(), "../../..", "data")
-    filename = "lorem_ipsum.txt"
-    print(most_common_words(Path(data_dir_path, filename), 7))
