2769099e00f11916231cf494e0edc4225c9b5ce2
doppelmarker <doppelmarker@gmail.com>
2021-09-28 14:21:16 +0300

    do task7_1

---
+++ MarkKanaplianik/task7_1/custom_file_context_manager.py
@@ -0,0 +1,40 @@
+"""
+Module contains a custom context manager class for working with files.
+
+Class:
+    File
+"""
+import sys
+import traceback
+
+
+def eprint(*args, **kwargs):
+    """Function to print in sys.stderr"""
+
+    print(*args, file=sys.stderr, **kwargs)
+
+
+class File:
+    def __init__(self, filename, mode="r"):
+        self.file = open(filename, mode)
+
+    def __enter__(self):
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is not None:
+            eprint("Traceback:")
+            traceback.print_tb(exc_tb)
+            eprint(f"{exc_type.__name__} was thrown inside {File.__name__} context manager: {exc_val}!")
+            self.file.close()
+            eprint(f"File descriptor was closed correctly!")
+            return True
+
+
+def main():
+    with File("file.txt", "w") as file:
+        file.write("Written from inside of custom context manager.").meme()
+
+
+if __name__ == "__main__":
+    main()
