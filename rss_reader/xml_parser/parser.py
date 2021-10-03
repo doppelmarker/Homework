748515a940a654182e4be90df0a720de8d290fe7
doppelmarker <doppelmarker@gmail.com>
2021-10-03 23:57:56 +0300

    make sure tokenizer IO gets closed properly

--- rss_reader/xml_parser/parser.py
+++ rss_reader/xml_parser/parser.py
@@ -13,23 +13,27 @@ class Parser:
 
     def parse(self):
         tokenizer = Tokenizer(self.xml)
+        try:
+            elementStack = deque()
 
-        elementStack = deque()
-
-        for token in tokenizer:
-            if tokenizer.token_type == TokenType.START_TAG:
-                if len(elementStack) != 0:
+            for token in tokenizer:
+                if tokenizer.token_type == TokenType.START_TAG:
+                    if len(elementStack) != 0:
+                        elementStack[-1].children.append(token)
+                        token.parent = elementStack[-1]
+                    elementStack.append(token)
+                elif tokenizer.token_type == TokenType.END_TAG:
+                    if len(elementStack) > 1:
+                        elementStack.pop()
+                elif tokenizer.token_type == TokenType.TEXT:
                     elementStack[-1].children.append(token)
                     token.parent = elementStack[-1]
-                elementStack.append(token)
-            elif tokenizer.token_type == TokenType.END_TAG:
-                if len(elementStack) > 1:
-                    elementStack.pop()
-            elif tokenizer.token_type == TokenType.TEXT:
-                elementStack[-1].children.append(token)
-                token.parent = elementStack[-1]
 
-        if len(elementStack) != 0:
-            return elementStack.pop()
-        else:
-            raise EmptyXMLDocumentError("empty xml document")
+            if len(elementStack) != 0:
+                return elementStack.pop()
+            else:
+                raise EmptyXMLDocumentError("empty xml document")
+        except XMLError:
+            pass
+        finally:
+            tokenizer.xml.close()
