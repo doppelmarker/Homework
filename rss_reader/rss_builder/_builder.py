cb097b65852f4ac90b2dd45328721dcedef14388
doppelmarker <doppelmarker@gmail.com>
2021-10-04 23:47:18 +0300

    make rss_items to be builded based on its model's fields

--- rss_reader/rss_builder/_builder.py
+++ rss_reader/rss_builder/_builder.py
@@ -1,4 +1,4 @@
-from rss_reader.rss_builder.rss_models import Feed
+from rss_reader.rss_builder.rss_models import Feed, Item
 
 
 class RSSBuilder:
@@ -8,22 +8,27 @@ class RSSBuilder:
 
     def build_feed(self) -> Feed:
         items = list(self.dom.find_all("item"))
+
         if self.limit:
             items = items[: self.limit]
 
-        data = {
+        item_fields = Item.__fields__.keys()
+
+        rss_items = []
+
+        for i, item in enumerate(items, start=1):
+            item_data = {"id": i}
+            for item_field in item_fields:
+                found_item = item.find(item_field)
+                if found_item:
+                    item_data[item_field] = found_item.next_text
+            rss_items.append(Item(**item_data))
+
+        feed_data = {
             "title": self.dom.find("title").next_text,
             "description": self.dom.find("description").next_text,
             "link": self.dom.find("link").next_text,
-            "items": [
-                {
-                    "id": i,
-                    "title": item.find("title").next_text,
-                    "link": item.find("link").next_text,
-                    "pub_date": item.find("pubDate").next_text,
-                }
-                for i, item in enumerate(items, start=1)
-            ],
+            "items": rss_items,
         }
 
-        return Feed(**data)
+        return Feed(**feed_data)
