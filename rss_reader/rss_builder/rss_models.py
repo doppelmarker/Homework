caf636746005c75ddf90b34332eda12b54475318
doppelmarker <doppelmarker@gmail.com>
2021-10-04 23:45:15 +0300

    make item to be printed only with fields which are present in xml

--- rss_reader/rss_builder/rss_models.py
+++ rss_reader/rss_builder/rss_models.py
@@ -9,7 +9,7 @@ class Item(BaseModel):
     description: Optional[str] = None
     link: Optional[str]
     author: Optional[str]
-    pub_date: Optional[str]
+    pubDate: Optional[str]
 
     @root_validator
     def either_title_or_description(cls, values):
@@ -20,7 +20,14 @@ class Item(BaseModel):
         return values
 
     def __str__(self):
-        return f"Item {self.id}\n  Title: {self.title}\n  Date: {self.pub_date}\n  Link: {self.link}"
+        fields = self.__dict__
+        result = ""
+        for field in fields:
+            if field == "id":
+                result += f"Item {fields[field]}\n  "
+            elif fields[field] is not None:
+                result += f"{field.capitalize()}: {fields[field]}\n  "
+        return result
 
 
 class Feed(BaseModel):
