e15c0eee8d0dcc498ee58d951f1bc2f9e513dad5
doppelmarker <doppelmarker@gmail.com>
2021-10-05 13:33:06 +0300

    make benefit of using generators instead of converting all items to list

--- rss_reader/rss_builder/_builder.py
+++ rss_reader/rss_builder/_builder.py
@@ -2,21 +2,24 @@ from rss_reader.rss_builder.rss_models import Feed, Item
 
 
 class RSSBuilder:
-    def __init__(self, dom, limit=None):
+    def __init__(self, dom, limit=-1):
         self.dom = dom
         self.limit = limit
 
     def build_feed(self) -> Feed:
-        items = list(self.dom.find_all("item"))
-
-        if self.limit:
-            items = items[: self.limit]
+        items = self.dom.find_all("item")
 
         item_fields = Item.__fields__.keys()
 
         rss_items = []
 
-        for i, item in enumerate(items, start=1):
+        def num_gen(limit):
+            i = 1
+            while i != limit:
+                yield i
+                i += 1
+
+        for i, item in zip(num_gen(self.limit), items):
             item_data = {"id": i}
             for item_field in item_fields:
                 found_item = item.find(item_field)
